0a7699a8fb64483cfff7b39414e7ad6b6302ad2b
zhtoo <820159571@qq.com>
2019-11-13 09:26:28 +0800

    add

---
+++ app/src/main/java/com/zht/zxingcode/CaptureActivity.java
@@ -0,0 +1,304 @@
+package com.zht.zxingcode;
+
+
+import android.app.Activity;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.Result;
+import com.zht.zxingcode.QRcode.AmbientLightManager;
+import com.zht.zxingcode.QRcode.BeepManager;
+import com.zht.zxingcode.QRcode.CaptureHandler;
+
+import com.zht.zxingcode.QRcode.FinishListener;
+import com.zht.zxingcode.QRcode.InactivityTimer;
+
+import com.zht.zxingcode.QRcode.ViewfinderView;
+import com.zht.zxingcode.QRcode.camera.CameraManager;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+
+public class CaptureActivity extends Activity
+        implements SurfaceHolder.Callback {
+
+    private static final String TAG = "CaptureActivity";
+    private boolean hasSurface;//是否持有Surface
+    private CameraManager cameraManager;//这里zxing是使用自己的CameraManager类
+    private CaptureHandler handler;
+
+    private ViewfinderView viewfinderView;
+    private Result lastResult;
+    private Collection<BarcodeFormat> decodeFormats;
+    private String characterSet;
+    private InactivityTimer inactivityTimer;
+    private BeepManager beepManager;
+    private AmbientLightManager ambientLightManager;
+
+    private Map<DecodeHintType, ?> decodeHints;
+
+    public ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    public CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        //保持屏幕常亮
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //设置透明状态栏
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        setContentView(R.layout.activity_main);
+
+        hasSurface = false;
+        inactivityTimer = new InactivityTimer(this);
+        beepManager = new BeepManager(this);
+        ambientLightManager = new AmbientLightManager(this);
+
+        View mStatusBarReplacement = findViewById(R.id.vStatusBarReplacement);
+        LinearLayout.LayoutParams layoutParams =
+                (LinearLayout.LayoutParams) mStatusBarReplacement.getLayoutParams();
+        layoutParams.height = getStatusHeightIfNeed(mStatusBarReplacement.getContext());
+        mStatusBarReplacement.setLayoutParams(layoutParams);
+        findViewById(R.id.qrcode_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        int height = resources.getDimensionPixelSize(resourceId);
+        return height;
+    }
+
+    /**
+     * 获取手机状态栏的高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusHeightIfNeed(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            return getStatusBarHeight(context);
+        }
+        return 0;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        cameraManager = new CameraManager(getApplication());
+        viewfinderView = (ViewfinderView) findViewById(R.id.viewfinder_view);
+        viewfinderView.setCameraManager(cameraManager);
+
+        handler = null;
+        resetStatusView();
+        beepManager.updatePrefs();
+        ambientLightManager.start(cameraManager);
+        inactivityTimer.onResume();
+        decodeFormats = null;
+        characterSet = null;
+
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            //activity已暂停但未停止，所以surface仍然存在。
+            // 因此不会调用surfaceCreated（），在此处只需初始化摄影机。
+            initCamera(surfaceHolder);
+        } else {
+            //添加回调并等待surfaceCreated（）初始化相机。
+            surfaceHolder.addCallback(this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        inactivityTimer.onPause();
+        ambientLightManager.stop();
+        beepManager.close();
+        cameraManager.closeDriver();
+        //historyManager = null; // Keep for onActivityResult
+        if (!hasSurface) {
+            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+            SurfaceHolder surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.removeCallback(this);
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        inactivityTimer.shutdown();
+        super.onDestroy();
+    }
+
+    /**
+     * 初始化相机
+     *
+     * @param surfaceHolder
+     */
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+        if (cameraManager.isOpen()) {
+            Log.w(TAG, "initCamera() while already open -- late SurfaceView callback?");
+            return;
+        }
+        try {
+            cameraManager.openDriver(surfaceHolder);
+
+            // Creating the handler starts the preview, which can also throw a RuntimeException.
+            if (handler == null) {
+                handler = new CaptureHandler(this,
+                        decodeFormats,
+                        decodeHints,
+                        characterSet,
+                        cameraManager);
+            }
+        } catch (IOException ioe) {
+            Log.w(TAG, ioe);
+            displayFrameworkBugMessageAndExit();
+        } catch (RuntimeException e) {
+            Log.w(TAG, "Unexpected error initializing camera", e);
+            displayFrameworkBugMessageAndExit();
+        }
+    }
+
+    private void displayFrameworkBugMessageAndExit() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.app_name));
+        builder.setMessage(getString(R.string.msg_camera_framework_bug));
+        builder.setPositiveButton(R.string.button_ok, new FinishListener(this));
+        builder.setOnCancelListener(new FinishListener(this));
+        builder.show();
+    }
+
+    /**
+     * surface被创建
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (holder == null) {
+            Log.e(TAG, "*** WARNING *** " + "surfaceCreated() gave us a null surface!");
+        }
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    /**
+     * surface发生改变
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // do nothing
+    }
+
+    /**
+     * surface被销毁
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    /**
+     * A valid barcode has been found, so give an indication of success and show the results.
+     *
+     * @param rawResult   The contents of the barcode.
+     * @param scaleFactor amount by which thumbnail was scaled
+     * @param barcode     A greyscale bitmap of the camera data which was decoded.
+     */
+    public void handleDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+        inactivityTimer.onActivity();
+        lastResult = rawResult;
+
+        String text = lastResult.getText();
+
+        if (TextUtils.isEmpty(text)) {
+            return;
+        }
+        //播放声音
+        beepManager.playBeepSoundAndVibrate();
+        // TODO: 2019/11/12 获取到结果
+        displayResult(text);
+    }
+
+    private void displayResult(String resultText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.app_name));
+        builder.setMessage(resultText);
+        builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                restartPreviewAfterDelay(0L);
+            }
+        });
+        builder.setOnCancelListener(new FinishListener(this));
+        builder.show();
+    }
+
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+    }
+
+    public void restartPreviewAfterDelay(long delayMS) {
+        if (handler != null) {
+            handler.sendEmptyMessageDelayed(R.id.restart_preview, delayMS);
+        }
+        resetStatusView();
+    }
+
+    private void resetStatusView() {
+        viewfinderView.setVisibility(View.VISIBLE);
+        lastResult = null;
+    }
+
+}
---
+++ app/src/main/java/com/zht/zxingcode/QRcode/config/ConfigManager.java
@@ -0,0 +1,155 @@
+package com.zht.zxingcode.QRcode.config;
+
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ZhangHaitao on 2019/11/8
+ */
+public class ConfigManager {
+
+    //一维码（商品）
+    public static final String KEY_DECODE_1D_PRODUCT = "preferences_decode_1D_product";
+    //一维码（工业）
+    public static final String KEY_DECODE_1D_INDUSTRIAL = "preferences_decode_1D_industrial";
+    //二维码
+    public static final String KEY_DECODE_QR = "preferences_decode_QR";
+    //Data Matrix
+    public static final String KEY_DECODE_DATA_MATRIX = "preferences_decode_Data_Matrix";
+    //Aztec
+    public static final String KEY_DECODE_AZTEC = "preferences_decode_Aztec";
+    //PDF417（测试）
+    public static final String KEY_DECODE_PDF417 = "preferences_decode_PDF417";
+
+    //自定义搜索网址
+    public static final String KEY_CUSTOM_PRODUCT_SEARCH = "preferences_custom_product_search";
+
+    //发出哔声
+    public static final String KEY_PLAY_BEEP = "preferences_play_beep";
+    //震动
+    public static final String KEY_VIBRATE = "preferences_vibrate";
+    //复制到剪切板
+    public static final String KEY_COPY_TO_CLIPBOARD = "preferences_copy_to_clipboard";
+    //闪光灯模式（是否开启闪光灯）
+    public static final String KEY_FRONT_LIGHT_MODE = "preferences_front_light_mode";
+    //批量扫描模式
+    public static final String KEY_BULK_MODE = "preferences_bulk_mode";
+    //保留重复记录
+    public static final String KEY_REMEMBER_DUPLICATES = "preferences_remember_duplicates";
+    //存入历史记录
+    public static final String KEY_ENABLE_HISTORY = "preferences_history";
+    //检索更多信息
+    public static final String KEY_SUPPLEMENTAL = "preferences_supplemental";
+    //自动对焦
+    public static final String KEY_AUTO_FOCUS = "preferences_auto_focus";
+    //反色（适用于黑背景白条码）
+    public static final String KEY_INVERT_SCAN = "preferences_invert_scan";
+    //搜索引擎国别
+    public static final String KEY_SEARCH_COUNTRY = "preferences_search_country";
+    //不自动旋转
+    public static final String KEY_DISABLE_AUTO_ORIENTATION = "preferences_orientation";
+    //不持续对焦（使用标准对焦模式）
+    public static final String KEY_DISABLE_CONTINUOUS_FOCUS = "preferences_disable_continuous_focus";
+    //不曝光
+    public static final String KEY_DISABLE_EXPOSURE = "preferences_disable_exposure";
+    //不使用距离测量
+    public static final String KEY_DISABLE_METERING = "preferences_disable_metering";
+    //不进行条形码场景匹配
+    public static final String KEY_DISABLE_BARCODE_SCENE_MODE = "preferences_disable_barcode_scene_mode";
+    //自动打开网页
+    public static final String KEY_AUTO_OPEN_WEB = "preferences_auto_open_web";
+
+    private Map<String, Object> configList;
+
+    private ConfigManager() {
+        initDefaultConfig();
+    }
+
+    private static ConfigManager mInstance;
+
+    public static ConfigManager getInstance() {
+        synchronized (ConfigManager.class) {
+            if (mInstance == null) {
+                mInstance = new ConfigManager();
+            }
+            return mInstance;
+        }
+    }
+
+    private void initDefaultConfig() {
+        configList = new HashMap<>();
+//      //一维码（商品）
+        configList.put(KEY_DECODE_1D_PRODUCT, new Boolean(true));
+//      //一维码（工业）
+        configList.put(KEY_DECODE_1D_INDUSTRIAL, new Boolean(true));
+//      //二维码
+        configList.put(KEY_DECODE_QR, new Boolean(true));
+//      //Data Matrix
+        configList.put(KEY_DECODE_DATA_MATRIX, new Boolean(true));
+//      //Aztec
+        configList.put(KEY_DECODE_AZTEC, new Boolean(false));
+//      //PDF417（测试）
+        configList.put(KEY_DECODE_PDF417, new Boolean(false));
+//      //自定义搜索网址
+//      configList.put(KEY_CUSTOM_PRODUCT_SEARCH, new Boolean(false));
+//      //发出声音
+        configList.put(KEY_PLAY_BEEP, new Boolean(true));
+//      //震动
+        configList.put(KEY_VIBRATE, new Boolean(false));
+//      //复制到剪切板
+//      configList.put(KEY_COPY_TO_CLIPBOARD, new Boolean(true));
+//      //闪光灯模式（是否开启闪光灯）
+        configList.put(KEY_FRONT_LIGHT_MODE, "OFF");
+//      //批量扫描模式
+//      configList.put(KEY_BULK_MODE, new Boolean(false));
+//      //保留重复记录
+//      configList.put(KEY_REMEMBER_DUPLICATES, new Boolean(false));
+//      //存入历史记录
+//      configList.put(KEY_ENABLE_HISTORY, new Boolean(true));
+//      //检索更多信息
+//      configList.put(KEY_SUPPLEMENTAL, new Boolean(true));
+//      //自动对焦
+        configList.put(KEY_AUTO_FOCUS, new Boolean(true));
+//      //反色（适用于黑背景白条码）
+        configList.put(KEY_INVERT_SCAN, new Boolean(false));
+//      //搜索引擎国别
+//      configList.put(KEY_SEARCH_COUNTRY, "-");
+//      //不自动旋转
+//      configList.put(KEY_DISABLE_AUTO_ORIENTATION, new Boolean(false));
+//      //不持续对焦（使用标准对焦模式）
+        configList.put(KEY_DISABLE_CONTINUOUS_FOCUS, new Boolean(false));
+//      //不曝光
+        configList.put(KEY_DISABLE_EXPOSURE, new Boolean(true));
+//      //不使用距离测量
+        configList.put(KEY_DISABLE_METERING, new Boolean(true));
+//      //不进行条形码场景匹配
+        configList.put(KEY_DISABLE_BARCODE_SCENE_MODE, new Boolean(true));
+//      //自动打开网页
+//      configList.put(KEY_AUTO_OPEN_WEB, new Boolean(false));
+
+
+    }
+
+    private Map<String, Object> getConfigList() {
+        return configList;
+    }
+
+    public static String getString(String key, @Nullable String defValue) {
+        Object value = getInstance().getConfigList().get(key);
+        if (value != null && value instanceof String) {
+            return (String) value;
+        }
+        return defValue;
+    }
+
+    public static Boolean getBoolean(String key, @Nullable boolean defValue) {
+        Object value = getInstance().getConfigList().get(key);
+        if (value != null && value instanceof Boolean) {
+            return (Boolean) value;
+        }
+        return defValue;
+    }
+
+}
